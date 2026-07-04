0788eacefe1e6f3a22323b0971af4363e7a07fd4
Johnny <52709518+1starexpert@users.noreply.github.com>
2026-07-04 16:39:05 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """ 
+        Brute force solution
+        """
+        max_profit = 0
+        for i in range(len(prices)):
+            for j in range(i + 1, len(prices)):
+                if prices[j] - prices[i] > max_profit:
+                    max_profit = prices[j] - prices[i]
+        return max_profit
+        
+
+
+        
